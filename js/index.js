0ae3fafdb6bafeb99f4324c9de3fc3125ee3af29
Darymee <danieledaniele3005@gmail.com>
2025-07-20 23:55:52 +0200

    HW-6

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const openModalBtn = document.querySelector('.button-hero');
 const closeModalBtn = document.querySelector('.close-modal-button');
 const modalOverlay = document.querySelector('.modal-overlay');
+const menuBtn = document.querySelector('.menu-button');
+const mobMenu = document.querySelector('.mobile-menu');
+const closeMenuBtn = document.querySelector('.mobile-menu-close');
 
 openModalBtn.addEventListener('click', () => {
   modalOverlay.classList.add('is-open');
@@ -24,4 +27,14 @@ document.addEventListener('keydown', e => {
     modalOverlay.classList.remove('is-open');
     document.body.classList.remove('modal-open');
   }
+});
+
+menuBtn.addEventListener('click', () => {
+  mobMenu.classList.add('is-open');
+  document.body.classList.add('modal-open');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  mobMenu.classList.remove('is-open');
+  document.body.classList.remove('modal-open');
 });
